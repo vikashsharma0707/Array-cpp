b8f31a5edcedcaad6bf70f174d42fe9c838a0b63
vikash sharma <vikashtiwari9893@gmail.com>
2024-10-01 00:40:25 +0530

    Array even sum

---
+++ arrayevensum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"enter number :";
+	cin>>n;
+	int arr[n];
+	int sum=0;
+	
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	
+		for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	
+	
+	
+	for(int i=0;i<n;i++){
+		if(arr[i]%2==0){
+			
+		sum=sum+arr[i];
+		}
+		cout<<sum;
+		
+	}
+		
+
+}
