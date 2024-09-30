85d7a769bcce767dd20b7208ea3939eff87470f5
vikash sharma <vikashtiwari9893@gmail.com>
2024-10-01 00:47:02 +0530

    array even and odd sum

---
+++ arrayevenoddsum.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"enter number";
+	cin>>n;
+	
+	int arr[n];
+	int  even=0;
+	int odd=0;
+	
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	
+	for(int i=0;i<n;i++){
+	     cout<<arr[i]<<" ";
+	}
+	
+	for(int i=0;i<n;i++){
+		if(arr[i]%2==0){
+			even=even+arr[i];
+		}
+		
+		else{
+			odd =odd+arr[i];
+		}
+	}
+	
+	cout<<"sum of even sum is :"<<even;
+	cout<<"sum of even odd is :"<<odd;
+}
