e6ee59de1555685129f0c3da7a3299baad1cb6a7
vikash sharma <vikashtiwari9893@gmail.com>
2024-10-01 01:22:54 +0530

    smallesr element in array

---
+++ arraysmallest.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"enter number ";
+	cin>>n;
+	
+	int arr[n];
+    int min =arr[0];
+    
+    
+    for(int i=0;i<n;i++){
+    	cin>>arr[i];
+	}
+	
+	 for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+	
+	 for(int i=1;i<n;i++){
+    	if(arr[i]<min){
+    		min=arr[i];
+		}
+	}
+	cout<<"minimum number is :"<<min;
+}
