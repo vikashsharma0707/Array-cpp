4cf47719d8f3be46a48fe1f97d261ca5341252ed
vikash sharma <vikashtiwari9893@gmail.com>
2024-10-02 09:11:01 +0530

    Array update

---
+++ arrayupdate1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Enter the number of array";
+	cin>>n;
+	
+	int arr[n];
+	int newele,index;
+	
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	
+	cout<<"Before updating elemnet "<<endl;
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	
+	cout<<"Enter the new element"<<endl;
+	cin>>newele;
+	cout<<"Enter the new index"<<endl;
+	cin>>index;
+	
+	arr[index]=newele;
+	cout<<"\nArray after updating the element"<<endl;
+	
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	
+	
+	
+	
+}
